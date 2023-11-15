25f81f5c4a32873916588a31ebd93a51444f02a1
GeronimoYiansens <130575851+GeronimoYiansens@users.noreply.github.com>
2023-11-15 15:57:30 -0300

    a

--- Obligatorio2MY/src/Interfaz/VentanaPostulante2.java
+++ Obligatorio2MY/src/Interfaz/VentanaPostulante2.java
@@ -263,8 +263,4 @@ public class VentanaPostulante2 extends javax.swing.JFrame{
     private javax.swing.JSpinner numeroNivel;
     // End of variables declaration//GEN-END:variables
 
-    @Override
-    public void update(Observable o, Object arg) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
